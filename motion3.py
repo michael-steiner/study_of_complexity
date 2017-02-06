f3d9e5bb57eb362506360268a53a644bf19b6bbf
michael-steiner <msteiner.a@gmail.com>
2017-02-05 20:49:21 -0800

    cleaned up and added interaction method

---
+++ motion3.py
@@ -0,0 +1,110 @@
+import numpy as np
+import pandas as pd
+import random
+import collections
+import math
+from enum import Enum
+
+
+class TypeOfParticle(Enum):
+    particle_zero = 0
+    particle_one = 1
+
+
+class Atom:
+    numeric_label = 0
+
+    def __init__(self):
+        self.location = (random.randint(-100, 100), random.randint(-100, 100))
+        self.numeric_label = Atom.numeric_label
+        Atom.numeric_label += 1
+
+    def simple_random_motion(self):
+        x = random.randint(-1, 1)
+        y = random.randint(-1, 1)
+        self.location = (self.location[0] + x, self.location[1] + y)
+
+    def check_distance(self, other):
+        return math.sqrt((((self.location[0] - other.location[0]) ** 2) + (self.location[1] - other.location[1]) ** 2))
+
+    def force_one(self):
+        pass
+
+    @property
+    def location(self):
+        return self.__location
+
+    @location.setter
+    def location(self, location):
+        self.__location = location
+
+
+class ParticleZero(Atom):
+    def __init__(self):
+        Atom.__init__(self)
+        self.type_of_particle = TypeOfParticle.particle_zero
+
+    def check_for_interactions(self, other):
+        zero_interaction = False
+        if other.type_of_particle == TypeOfParticle.particle_zero:
+            if np.random.randint(0, 100000) <= 100:
+                zero_interaction = True
+        return zero_interaction
+
+
+class ParticleOne(Atom):
+    def __index__(self):
+        Atom.__init__(self)
+        self.type_of_particle = TypeOfParticle.particle_one
+
+
+class Universe:
+    def __init__(self, eot):
+        self.zero_particles = collections.deque()
+        self.one_particles = collections.deque()
+        self.end_of_time = eot
+        self.record_of_the_state_of_the_universe = pd.DataFrame(np.nan, index=[x for x in range(self.end_of_time)],
+                                                                columns=['time', 'zero_particles', 'one_particles'])
+
+    def move_zero_particles(self):
+        """ this method may be slow because we are appending on the left - consider writing the 'list' data structure"""
+        set_of_particle_x_change_to_particle_y = set()
+
+        for particle_type_zero_move_counter in range(len(self.zero_particles)):
+            zero_particle_to_move = self.zero_particles.pop()
+            zero_particle_to_move.simple_random_motion()
+            for zero_particle_to_check_proximity in self.zero_particles:
+                if zero_particle_to_move.check_distance(zero_particle_to_check_proximity) < 10:
+                    if zero_particle_to_move.check_for_interactions(zero_particle_to_check_proximity):
+                        set_of_particle_x_change_to_particle_y.add(zero_particle_to_move.numeric_label)
+                        set_of_particle_x_change_to_particle_y.add(zero_particle_to_check_proximity.numeric_label)
+            self.zero_particles.appendleft(zero_particle_to_move)
+
+        for zero_particle_interaction_counter in range(len(self.zero_particles)):
+            zero_particle_to_categorize = self.zero_particles.pop()
+            if zero_particle_to_categorize.numeric_label in set_of_particle_x_change_to_particle_y:
+                self.one_particles.append(zero_particle_to_categorize)
+            else:
+                self.zero_particles.appendleft(zero_particle_to_categorize)
+
+    def bang(self, number_of_zero_particles):
+        age_of_universe = 0
+
+        for particle in range(number_of_zero_particles):
+            self.zero_particles.append(ParticleZero())
+
+        while age_of_universe <= self.end_of_time:
+            self.record_of_the_state_of_the_universe.ix[age_of_universe, 'time'] = age_of_universe
+            self.record_of_the_state_of_the_universe.ix[age_of_universe, 'zero_particles'] = len(self.zero_particles)
+            self.record_of_the_state_of_the_universe.ix[age_of_universe, 'one_particles'] = len(self.one_particles)
+
+            self.move_zero_particles()
+
+            age_of_universe += 1
+
+
+test5 = True
+if test5:
+    thisUniverse = Universe(100)
+    thisUniverse.bang(100)
+    print(thisUniverse.record_of_the_state_of_the_universe)
